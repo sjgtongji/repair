3f3b74e3075d57b1003355d82a6e486255328ef2
jigang Sun <sjgtongji@126.com>
2018-06-07 18:31:24 +0800

    use material ui

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,16 +1,29 @@
 import React, {Component} from 'react';
 import {hot} from 'react-hot-loader';
-import {Button} from 'antd';
+import IconButton from '@material-ui/core/IconButton';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import { withStyles } from '@material-ui/core/styles';
 var css = require('../../css/Home.css')
 var common = require('../../css/common.css')
 var classNames = require('classnames');
 import * as axios from '../../util/AxiosUtil';
-
 class Home extends Component {
+		state = {
+			password: ''
+		};
     constructor(props) {
         super(props);
         this.state = {
-            count: 0
+            count: 0,
+						showPassword: false
         }
     }
 
@@ -20,27 +33,56 @@ class Home extends Component {
         });
 				//js实现跳转
 				// this.props.history.push('/page1');
-        axios.post('loginByPhoneNum',
-          {
-            phoneNum:123456789,
-            pwd:9999,
-            openId:1111
-          },
-          (res)=>{
-            console.log(res);
-          });
+        // axios.post('loginByPhoneNum',
+        //   {
+        //     phoneNum:123456789,
+        //     pwd:9999,
+        //     openId:1111
+        //   },
+        //   (res)=>{
+        //     console.log(res);
+        //   });
         // axios.get('loginByOpenId?openId=1111' ,
         //   (res) =>{
         //
         //   });
     }
 
+		handleChange = prop => event => {
+	    this.setState({ [prop]: event.target.value });
+	  };
+
+	  handleMouseDownPassword = event => {
+	    event.preventDefault();
+	  };
+
+	  handleClickShowPassword = () => {
+	    this.setState({ showPassword: !this.state.showPassword });
+	  };
+
     render() {
         return (
-            <div className={classNames(css.root, common.flex, common.vertical, common.justfycenter, common.alignend)}>
-                this is home~<br/>
-                当前计数：{this.state.count}<br/>
-                <Button onClick={() => this._handleClick()}>自增</Button>
+            <div className={classNames(css.root, common.flex, common.vertical, common.justfycenter, common.aligncenter)}>
+						<FormControl className={classNames(styles.margin, styles.textField)}>
+							<InputLabel htmlFor="adornment-password">Password</InputLabel>
+							<Input
+								id="adornment-password"
+								type={this.state.showPassword ? 'text' : 'password'}
+								value={this.state.password}
+								onChange={this.handleChange('password')}
+								endAdornment={
+									<InputAdornment position="end">
+										<IconButton
+											aria-label="Toggle password visibility"
+											onClick={this.handleClickShowPassword}
+											onMouseDown={this.handleMouseDownPassword}
+										>
+											{this.state.showPassword ? <VisibilityOff /> : <Visibility />}
+										</IconButton>
+									</InputAdornment>
+								}
+							/>
+						</FormControl>
             </div>
         )
     }
@@ -78,5 +120,34 @@ class Home extends Component {
     //
     // }
 }
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  margin: {
+    margin: theme.spacing.unit,
+  },
+  withoutLabel: {
+    marginTop: theme.spacing.unit * 3,
+  },
+  textField: {
+    flexBasis: 200,
+  },
+});
 
+const ranges = [
+  {
+    value: '0-20',
+    label: '0 to 20',
+  },
+  {
+    value: '21-50',
+    label: '21 to 50',
+  },
+  {
+    value: '51-100',
+    label: '51 to 100',
+  },
+];
 export default hot(module)(Home);
