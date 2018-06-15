3b18c6327d16856f44296bf7d34a2bc02d0e4355
jigang Sun <sjgtongji@126.com>
2018-06-15 16:42:59 +0800

    Use styles instead of css in Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,13 +3,12 @@ import {hot} from 'react-hot-loader';
 import {Link} from 'react-router-dom';
 import FlexInput from 'cusComponents/FlexInput';
 import Button from 'cusComponents/Button';
-var css = require('../../css/Home.css')
-var common = require('../../css/common.css')
 var classNames = require('classnames');
 import * as axios from '../../util/AxiosUtil';
 import * as Constant from '../../util/Constant';
 const sendExp = '发送验证码';
 const sendInterval = 60;
+import { withStyles } from '@material-ui/core/styles';
 class Home extends Component {
 		constructor(props) {
 				super(props);
@@ -62,21 +61,53 @@ class Home extends Component {
 		}
 
 		render() {
+			const {classes} = this.props;
 				return (
-						<div className={classNames(css.root, common.flex, common.vertical, common.justfystart, common.alignstretch)}>
-							<FlexInput ref = "phonenum" type='text' label='手机号' className={css.phonenum} onChange={(event) => this.setState({phonenum : event.target.value})}></FlexInput>
-							<div className={classNames(common.flex, common.justfyspacebetween, common.alignend,css.codeDiv)}>
-								<FlexInput ref = "code" label='验证码' type='text' className={css.code} onChange={(event) => this.setState({code : event.target.value})}></FlexInput>
-								<Button variant="contained" color="primary" className={css.send} disabled={this.state.sendDisabled} onClick={(event) => this.onSend()}>
+						<div className={classes.root}>
+							<FlexInput ref = "phonenum" type='text' label='手机号' onChange={(event) => this.setState({phonenum : event.target.value})}></FlexInput>
+							<div className={classes.codeDiv}>
+								<FlexInput ref = "code" label='验证码' type='text' className={classes.code} onChange={(event) => this.setState({code : event.target.value})}></FlexInput>
+								<Button variant="contained" color="primary" className={classes.send} disabled={this.state.sendDisabled} onClick={(event) => this.onSend()}>
 									{this.state.sendExp}
 								</Button>
 							</div>
-							<Button variant="contained" color="primary" className={css.login} onClick={(event) => this.onLogin()} disabled={this.state.loginDisabled}>
+							<Button variant="contained" color="primary" onClick={(event) => this.onLogin()} disabled={this.state.loginDisabled}>
 								登录
 							</Button>
-							<Link to="/userinfo" className={css.contact}>联系客服</Link>
+							<Link to="/userinfo" className={classes.contact}>联系客服</Link>
 						</div>
 				)
 		}
 }
-export default hot(module)(Home);
+const styles = theme => ({
+	root : {
+		display : 'flex',
+		flexDirection : 'column',
+		alignItems : 'stretch',
+		width : Constant.window.width * 0.8,
+		height: Constant.window.height * 0.9,
+		paddingTop : Constant.window.height * 0.1,
+		paddingLeft : Constant.window.width * 0.1,
+		paddingRight : Constant.window.width * 0.1,
+		overflowX : 'hidden'
+	},
+	codeDiv : {
+		display : 'flex',
+		justfyContent : 'space-between',
+		alignItems : 'flex-end',
+		marginTop: Constant.window.height * 0.05,
+		marginBottom: Constant.window.height * 0.1
+	},
+	code : {
+		width : Constant.window.width * 0.4
+	},
+	send : {
+		width : Constant.window.width * 0.4
+	},
+	contact : {
+		alignSelf : 'flex-end',
+		marginTop : Constant.window.height * 0.05,
+		fontSize: 32
+	}
+});
+export default hot(module)(withStyles(styles)(Home));
